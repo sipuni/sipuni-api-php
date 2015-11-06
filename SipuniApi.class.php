c21d50120bf0c171fbd7c63a2ecddf41c90d4f3c
Алексей Горобчук <ah@en.cx>
2015-11-06 20:35:39 +0300

    use httpful for requests

--- SipuniApi.class.php
+++ SipuniApi.class.php
@@ -10,30 +10,15 @@ class SipuniApi {
 
     private $apiKey;
     private $apiHost;
+    private $apiVersion = 'v1';
 
     function __construct($apiKey, $apiHost='api.sipuni.com'){
         $this->apiKey = $apiKey;
         $this->apiHost = $apiHost;
     }
 
-    public function get($apiMethodPath, array $args){
-        return array();
-    }
-
-    public function put($apiMethodPath, array $args){
-        return array();
-    }
-
-    public function post($apiMethodPath, array $args){
-        return array();
-    }
-
-    protected function getMethodUrl(){
-        return array();
-    }
-
-    protected function getHeaders(){
-        return array();
+    public function getMethodUrl($path){
+        return "https://{$this->apiHost}/{$this->apiVersion}{$path}?format=json";
     }
 
 }
--- example.php
+++ example.php
@@ -1,22 +1,43 @@
 <?php
 
+require_once ('httpful.phar');
 require_once ('SipuniApi.class.php');
 use sipuni\SipuniApi;
+
 
 define('SIPUNI_API_KEY', 'secret_key');
 define('SIPUNI_CUSTOMER_ID', '3');
 
-function main(){
 
+function allocate_number($purpose, $forward_to){
     $api = new SipuniApi(SIPUNI_API_KEY);
 
-    $result = $api->put('/api/calltracking/allocate_static/', array(
+    $args = array(
         'customer'=>SIPUNI_CUSTOMER_ID,
-        'purpose'=>'Client 23',
-        'forward_to'=>'+74991231212'
-    ));
+        'purpose'=>$purpose,
+        'forward_to'=>$forward_to
+    );
 
-    print $result;
+    $apiMethodUrl = $api->getMethodUrl('/calltracking/allocate_static/');
+
+    $response = \Httpful\Request::put($apiMethodUrl)
+        ->sendsJson()
+        ->expectsJson()
+        ->body(json_encode($args))
+        ->send();
+
+    return $response;
+}
+
+function main(){
+    $result = allocate_number('Client 23', '+74991231212' );
+    if($result->code==200){
+        if($result->body->success){
+            print ("Allocated number ".$result->body->number);
+        }else{
+            print ("Error: ".$result->body->msg);
+        }
+    }
 }
 
 main();
